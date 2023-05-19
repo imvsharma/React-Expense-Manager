a500aac23e31bafb373eb8813837b1cab6a6054d
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2023-05-19 15:47:24 +0530

    Added Expense Date Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2023, 3, 28)
+    },
+    {
+      id: 'e2',
+      title: 'Toilet Paper',
+      amount: 94.67,
+      date: new Date(2023, 5, 2)
+    },
+    {
+      id: 'e3',
+      title: 'New Desk',
+      amount: 294.67,
+      date: new Date(2023, 1, 21)
+    },
+    {
+      id: 'e4',
+      title: 'Laptop',
+      amount: 799.67,
+      date: new Date(2023, 3, 19)
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses expenses= {expenses} />
     </div>
   );
 }
