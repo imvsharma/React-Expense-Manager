fd96d5a8a327757af023300a915fe00b42fb5c96
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2023-05-19 15:44:00 +0530

    Added Expenses item component

---
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,18 @@
+import Card from '../Card/Card';
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem (props) {
+    const {date, title, amount} = props;
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={date} />
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>{amount}</div>
+            </div>
+        </Card>
+    )
+}
+
+export default ExpenseItem
